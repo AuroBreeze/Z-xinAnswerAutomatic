8bead8c0e29dba4ec415cffc1cd290267e11d0e6
AuroBreeze <1732373074@qq.com>
2024-11-15 20:48:33 +0800

    设置软件基本信息

--- Module/Introduce.py
+++ Module/Introduce.py
@@ -1,14 +1,18 @@
+import yaml
 from Module.Logger import Logger
 class Init_Introduce():
     def __init__(self):
+        with open('config.yml', 'r') as stream:
+            self.config = yaml.load(stream, Loader=yaml.FullLoader)
+
         self.logger = Logger()
-        self.logger.Message_Log_Info("欢迎使用Z-XinAnswerautomatic！")
-        self.logger.Message_Log_Info("作者：AuroBreeze")
-        self.logger.Message_Log_Info("版本：1.0.2")
-        self.logger.Message_Log_Info("日期：2024-11-09")
-        self.logger.Message_Log_Info("许可证：MIT")
+        self.logger.Message_Log_Info("欢迎使用Z-XinAnswerautomatic!")
+        self.logger.Message_Log_Info("作者：" + self.config["Software_Information"]['author'])
+        self.logger.Message_Log_Info("版本：" + self.config["Software_Information"]['version'])
+        self.logger.Message_Log_Info("日期：" + self.config["Software_Information"]['update'])
+        self.logger.Message_Log_Info("许可证：" + self.config["Software_Information"]['license'])
         self.logger.Message_Log_Info("GITHUB地址：https://github.com/AuroBreeze/Z-XinAnswerautomatic")
-        self.logger.Message_Log_Info("博客地址：https://blog.aurobreeze.top/")
+        self.logger.Message_Log_Info("博客地址：" + self.config["Software_Information"]['github'])
         self.logger.Message_Log_Info("此应用仅供学习交流使用，出现其他任何问题，与作者无关。")
         self.logger.Message_Log_Info("请按回车键继续...")
         input()
